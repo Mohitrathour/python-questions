0989b55f409b8c89f73c8f4d805ebadaf812a8de
Mohitrathour <rathourmohit145@gmail.com>
2026-01-15 21:03:23 +0530

    Add files via upload

---
+++ question1.py
@@ -0,0 +1,4 @@
+a=int(input("enter your no1"))
+b=int(input("enter your no2"))
+c=a+b
+print("sum of two no is",c)
---
+++ question2.py
@@ -0,0 +1,10 @@
+a=int(input("enter no 1"))
+b=int(input("enter no 2"))
+sum=a+b
+difference=a-b
+product=a*b
+quotient=a//b
+print("sum of two no is",sum)
+print("difference of two no is",difference)
+print("product of two no is",product)
+print("quotient of two no is",quotient)
