faf50324263611909779b93546591cc3f611be1d
Mohitrathour <rathourmohit145@gmail.com>
2026-01-16 13:26:38 +0530

    Add files via upload

---
+++ question3.py
@@ -0,0 +1,7 @@
+#Q3: Write a program to calculate the area and perimeter of a rectangle given its length and breath
+a=float(input("enter the length"))
+b=float(input("enter the breath"))
+perimeter=2*(a+b)
+area=a*b
+print("perimeter of the rectangle:",perimeter)
+print("area of the rectangle:",area)
---
+++ question4.py
@@ -0,0 +1,6 @@
+#Q4: Write a program to calculate the area and circumference of a circle given its radius.
+a=int(input("enter the radius"))
+circumference=2*3.14*a
+area=3.14*(a**2)
+print("circumference of the circle",circumference)
+print("area of thr circle",area)
