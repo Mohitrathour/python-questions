ff2ec926ca9e7fdc59d6d7adf09ee355cfa03fc1
Mohitrathour <rathourmohit145@gmail.com>
2026-01-16 13:29:32 +0530

    Refactor input and output for arithmetic operations

--- question2.py
+++ question2.py
@@ -1,3 +1,4 @@
+#Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.
 a=int(input("enter no 1"))
 b=int(input("enter no 2"))
 sum=a+b
@@ -8,3 +9,4 @@ print("sum of two no is",sum)
 print("difference of two no is",difference)
 print("product of two no is",product)
 print("quotient of two no is",quotient)
+
