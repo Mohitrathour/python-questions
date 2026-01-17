14ff9bedb2023658b5990cd2d1438abcdb943bc0
Mohitrathour <rathourmohit145@gmail.com>
2026-01-17 19:57:12 +0530

    Add files via upload

---
+++ question5.py
@@ -0,0 +1,4 @@
+#Q5: Write a program to convert temperature from Celsius to fahrenheit
+n=int(input("enter the no"))
+temp=(n*9/5)+32
+print("the converted temprature is:",temp)
---
+++ question6.py
@@ -0,0 +1,9 @@
+#Q6: Write a program to swap two numbers using a third variable.
+n=int(input("enter the no1"))
+m=int(input("enter the no2"))
+temp=0
+temp=n
+n=m
+m=temp
+print("swaped first no is:",n)
+print("swaped second no is:",m)
