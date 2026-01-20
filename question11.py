372ac6bd19d676a6c00e628f543e362aacd9a296
Mohit Singh Rathour <rathourmohit145@gmail.com>
2026-01-20 19:33:40 +0530

    Add files via upload

---
+++ question11.py
@@ -0,0 +1,7 @@
+#Q11: Write a program to input an integer and check whether it is even or odd using if–else.
+
+n=int(input("enter a no"))
+if(n%2==0):
+    print(n,"is even")
+else:
+    print(n,"is odd")
---
+++ question12.py
@@ -0,0 +1,8 @@
+#Q12: Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+n=int(input("enter a no"))
+if(n>0):
+    print("the no is positive")
+elif(n<0):
+    print("the no is negative")
+else:
+    print("the no is zero")
