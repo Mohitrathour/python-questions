2eac8b4333bb2d99dd9beefc0c47804b0783be2f
Mohit Singh Rathour <rathourmohit145@gmail.com>
2026-02-05 21:44:49 +0530

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,5 @@
+def sum():
+    n=int(input("enter 1 no"))
+    n2=int(input("enter 2 no"))
+    print(n+n1)
+sum()    
---
+++ function2.py
@@ -0,0 +1,6 @@
+def multiple():
+    n=int(input("enter first no"))
+    n2=int(input("enter second no"))
+    n3=int(input("enter thirst no"))
+    print("product:",n*n2*n3)
+multiple()    
