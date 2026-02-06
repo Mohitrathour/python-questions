abf445e0a041dfeac2c4cb10b48c28be87d3d2ba
Mohit Singh Rathour <rathourmohit145@gmail.com>
2026-02-06 23:03:14 +0530

    Add files via upload

---
+++ function3.py
@@ -0,0 +1,8 @@
+def calculate():
+    l=int(input("enter the length"))
+    b=int(input("enter the breath"))
+    area=l*b
+    perimeter=2*(l+b)
+    print("area:",area)
+    print("perimeter:",perimeter)
+calculate()    
---
+++ function4.py
@@ -0,0 +1,7 @@
+def circle():
+    r=int(input("enter the radius"))
+    area=3.14*(r**2)
+    circumference=2*3.14*r
+    print("area of circle:",area)
+    print("circumfrence of cicle:",circumference)
+circle()    
